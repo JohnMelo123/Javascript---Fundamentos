f97d68b6fb1a6c4777e17eb867d9a4e2bdf7d7ca
JohnMelo <132289758+JohnMelo123@users.noreply.github.com>
2026-08-30 19:20:24 -0300

    up

--- ARRAY/map3.js
+++ ARRAY/map3.js
@@ -1,10 +1,10 @@
-Array.prototype.map2 = function(callback, thisArrg) {
+Array.prototype.map2 = function(callback, thisArg) {
+    newArray = []
     for(let i = 0; i < this.length; i++) {
     newArray.push(callback(this[i], i, this))
     }
     return newArray
 }
-
 
 const carrinho = [
     '{ "nome": "Borracha", "preco": "3.34" }',

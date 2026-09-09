8e19471f6fa063d067d570400388e23451c167d5
JohnMelo <132289758+JohnMelo123@users.noreply.github.com>
2026-09-09 10:56:11 -0300

    up

--- exercicios2.js
+++ exercicios2.js
@@ -4,4 +4,36 @@ function saudacao (nome) {
 }
 console.log(saudacao("John Melo"))
 
+
 // Função que recebe idade em anos e retorna em dias:
+function converterIdade (idade) {
+    return idade * 365
+}
+console.log(converterIdade(15))
+console.log(converterIdade(21))
+
+
+// Função de calculo de salário:
+const calcularSalario = function (horas, salHora) {
+    return `Salário igual a R$ ${horas * salHora}`
+}
+console.log(calcularSalario(176, 32.6))
+console.log(calcularSalario(180, 50))
+
+
+// Função de mes do ano:
+function nomeDoMes (mes) {
+    let meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 
+    'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+    return meses[mes-1]   
+}
+console.log(nomeDoMes(12))
+
+
+// Função de teste Maior ou Igual:
+function maiorOuIgual (x, y) {
+    return x >= y? true:false
+}
+console.log(maiorOuIgual(5,20))
+console.log(maiorOuIgual(14, 7))
+console.log(maiorOuIgual('100', 22))

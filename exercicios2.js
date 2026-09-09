abfb5d59eb58f973ea3babb78931622f76e59308
JohnMelo <132289758+JohnMelo123@users.noreply.github.com>
2026-09-08 21:40:30 -0300

    up

--- exercicios2.js
+++ exercicios2.js
@@ -1,4 +1,7 @@
+// Função retornada com template string:
 function saudacao (nome) { 
     return 'Olá, ' + nome + '!'
 }
 console.log(saudacao("John Melo"))
+
+// Função que recebe idade em anos e retorna em dias:

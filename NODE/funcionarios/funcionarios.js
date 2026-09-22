c57c7279f65f0c173b5f689102b27ad0d530401c
JohnMelo <132289758+JohnMelo123@users.noreply.github.com>
2026-09-22 11:25:21 -0300

    up

--- NODE/funcionarios/funcionarios.js
+++ NODE/funcionarios/funcionarios.js
@@ -1,7 +1,28 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
+// Desafio Mulher chinesa com menor salário:
+
+const chineses = function (funcionarios) {
+    funcionarios.pais === 'China'
+}
+
+const mulheres = function (funcionarios) {
+    funcionarios.genero === 'F'
+}
+
+const menorSalario = function (func,funcAtual) {
+    return func.salario < funcAtual.salario ? func : funcAtual
+}
+
 axios.get(url).then(response => {
     const funcionarios = response.data
     console.log(funcionarios)
+
+    const func = funcionarios
+         .filter(chineses)
+         .filter(mulheres)
+         .filter(menorSalario)
+    console.log(func)
 })
+

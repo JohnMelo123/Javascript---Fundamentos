e33992cc4808219164974ee235dc5d1f2589b7f2
JohnMelo <132289758+JohnMelo123@users.noreply.github.com>
2026-09-22 21:25:06 -0300

    up

---
+++ NODE/global.js
@@ -0,0 +1,8 @@
+// console.log(global)
+
+globalThis.minhaApp = {
+    saudacao() {
+        return 'Estou em todos os lugares!'
+    },
+    nome: 'Sistema Legal'
+}
---
+++ NODE/globalCliente.js
@@ -0,0 +1,6 @@
+require('./global.js')
+
+console.log(minhaApp.saudacao())
+
+minhaApp.nome = 'Eita!'
+console.log(minhaApp.nome)
